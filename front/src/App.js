f958566b5387ccea4ca42ba1b82286e7d37477f1
mane <aplplac@gmail.com>
2020-12-28 18:40:28 -0700

    add initial commit of api and front

---
+++ front/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div className="App-container">
+          CHILTE-RED
+        </div>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
